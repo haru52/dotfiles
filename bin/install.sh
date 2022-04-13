3be34cd5fd1b24fed37b614a6f04d67447a42af2
haru <haru@haru52.com>
2022-04-14 00:34:58 +0900

    feat: update bin/install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -2,8 +2,8 @@
 
 dotfiles_path=$(cd $(dirname $0)/../dotfiles/ && pwd)
 
+cp -f $dotfiles_path/.zpreztorc ~/.zprezto/runcoms/zpreztorc
 cp -f $dotfiles_path/.zshrc ~/.zprezto/runcoms/zshrc
 
 ln -sfv $dotfiles_path/.gitignore_global ~
 ln -sfv $dotfiles_path/.vimrc ~
-ln -sfv $dotfiles_path/.zpreztorc ~
