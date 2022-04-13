72c2f7e0c5b754fb27b43558d8252ffda79b8f5c
haru <haru@haru52.com>
2022-04-14 00:13:49 +0900

    feat: update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -4,5 +4,5 @@ dotfiles_path=$(cd $(dirname $0)/../dotfiles/ && pwd)
 
 cp -f $dotfiles_path/.zshrc ~/.zprezto/runcoms/zshrc
 
-ln -sf $dotfiles_path/.gitignore_global ~
-ln -sf $dotfiles_path/.vimrc ~
+ln -sfv $dotfiles_path/.gitignore_global ~
+ln -sfv $dotfiles_path/.vimrc ~
