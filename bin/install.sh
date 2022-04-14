be650be79a1d2e5a1263333d771dfcf380677f6d
haru <haru@haru52.com>
2022-04-15 04:43:13 +0900

    feat(install.sh): change to output verbose logs while executing cp commands

--- bin/install.sh
+++ bin/install.sh
@@ -2,8 +2,8 @@
 
 dotfiles_path=$(cd $(dirname $0)/../dotfiles/ && pwd)
 
-cp -f $dotfiles_path/.zpreztorc ~/.zprezto/runcoms/zpreztorc
-cp -f $dotfiles_path/.zshrc ~/.zprezto/runcoms/zshrc
+cp -fv $dotfiles_path/.zpreztorc ~/.zprezto/runcoms/zpreztorc
+cp -fv $dotfiles_path/.zshrc ~/.zprezto/runcoms/zshrc
 
 ln -sfv $dotfiles_path/.gitignore_global ~
 ln -sfv $dotfiles_path/.vimrc ~
