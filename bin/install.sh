9aeba3c18218433c630c9a87a3f5d926ed82bf1a
haru <haru@haru52.com>
2022-07-15 22:07:24 +0900

    style: Fix style

--- bin/install.sh
+++ bin/install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-cd $(dirname $0)/../dotfiles || exit 1
+cd "$(dirname "$0")"/../dotfiles || exit 1
 
 dotfiles_path=$(pwd)
 
-ls_dotfiles=$(ls -a $dotfiles_path)
+ls_dotfiles=$(ls -a "$dotfiles_path")
 
+# shellcheck disable=SC2068
 for file_name in ${ls_dotfiles[@]}; do
-  file_path=$dotfiles_path/$file_name
-  if [ -f $file_path ]; then
-    ln -sfv $file_path ~
+  file_path=${dotfiles_path}/${file_name}
+  if [ -f "$file_path" ]; then
+    ln -sfv "$file_path" ~
   fi
 done
 
-cp -fv $dotfiles_path/zprezto/* ~/.zprezto/runcoms
+cp -fv "$dotfiles_path"/zprezto/* ~/.zprezto/runcoms
